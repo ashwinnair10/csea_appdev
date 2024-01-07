c5fbaf31512df84551218759e085099994fa2075
ashwinnair10 <ashwinnair1894@gmail.com>
2024-01-07 11:39:20 +0530

    mod1

---
+++ lib/box.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+Widget buildbox(BuildContext context, String str1, str2, str3, Color c) {
+  return Stack(
+    alignment: Alignment.bottomCenter,
+    children: [
+      Container(
+        height: 200,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: c,
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(30, 20, 20, 20),
+        height: 150,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color.fromARGB(255, 28, 31, 74),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  str1,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: () => {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  str2,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  str3,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
 
+import 'package:csea_appdev/box.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +14,74 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color.fromARGB(255, 15, 20, 36),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(30, 200, 30, 20),
+        child: Column(
+          children: [
+            buildbox(
+              context,
+              'Work',
+              '5 hrs',
+              'Yesterday - 7 hrs',
+              Colors.orange,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildbox(
+              context,
+              'Play',
+              '1 hrs',
+              'Yesterday - 2 hrs',
+              Colors.cyan,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildbox(
+              context,
+              'Study',
+              '0 hrs',
+              'Yesterday - 1 hrs',
+              Colors.pink,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildbox(
+              context,
+              'Exercise',
+              '1 hrs',
+              'Yesterday - 1 hrs',
+              Colors.green,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildbox(
+              context,
+              'Social',
+              '1 hrs',
+              'Yesterday - 3 hrs',
+              Colors.purple,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildbox(
+              context,
+              'Self-care',
+              '0 hrs',
+              'Yesterday - 1 hrs',
+              Colors.yellow,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
